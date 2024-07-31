58936b9046077590af26743b7a33165eeb541015
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-07-31 15:15:20 -0500

    comments

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -17,6 +17,13 @@ class Test extends Command
 
     public function handle(): void
     {
+        /**
+         * This code below grabs a user and an application submitted by the user.
+         * Using those values and the ChatRequestService class, we can build out
+         * the messages payload required for the Llama API to create a chat request.
+         * Using that built payload, we can utilize the Llama Connector to send the
+         * request to the Llama API and log the response.
+         */
         $user = User::query()->where('id', 1)->first();
         $application = Application::query()->where('id', 1)->first();
 
