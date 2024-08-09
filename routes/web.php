fb1f41d41e6ed26852f96d0277392649e182a142
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-08 22:18:13 -0500

    dashboard page

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => inertia('Landing/Index'))->name('index');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return inertia('Welcome');
-    })->name('dashboard');
+    Route::get('/dashboard', fn () => inertia('Dashboard/Index'))->name('dashboard');
 });
 
 Route::prefix('/register')->group(function () {
