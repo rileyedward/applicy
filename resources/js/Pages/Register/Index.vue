0165a4b60dfce83a8486e3595b5c07ada78e6512
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-08 22:07:22 -0500

    colors

--- resources/js/Pages/Register/Index.vue
+++ resources/js/Pages/Register/Index.vue
@@ -7,7 +7,7 @@ import RegistrationForm from '@/Pages/Register/Partials/RegistrationForm.vue';
     class="flex items-center justify-center min-h-screen px-4 bg-neutral-900 text-neutral-100"
   >
     <div class="bg-neutral-800 p-8 rounded-lg shadow-lg w-full max-w-3xl">
-      <h1 class="text-4xl font-bold text-center text-neutral-100 mb-6">
+      <h1 class="text-4xl font-bold text-center text-orange-600 mb-6">
         Applicy
       </h1>
       <p class="text-center text-neutral-400 mb-8">
