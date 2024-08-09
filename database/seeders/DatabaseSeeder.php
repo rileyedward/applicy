0f3af8f37e7235237680412650dd502afbcfc101
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-09 00:03:01 -0500

    db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\EducationExperience;
+use App\Models\PortfolioProject;
+use App\Models\ProfessionalExperience;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $user = User::factory()->create([
+            'first_name' => 'Dev',
+            'last_name' => 'Admin',
+            'email' => 'dev@applicy.com',
+        ]);
+
+        EducationExperience::factory()->for($user)->create();
+
+        ProfessionalExperience::factory()->for($user)->create();
+
+        PortfolioProject::factory()->for($user)->create();
     }
 }
