69cdd97443c91c20c1b952c18827f3e7de21016c
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-10 15:14:42 -0500

    wip

--- resources/js/Pages/Applications/Index.vue
+++ resources/js/Pages/Applications/Index.vue
@@ -1,11 +1,16 @@
 <script setup>
 import MainLayout from '@/Layouts/MainLayout.vue';
+import JobApplicationModal from '@/Pages/Applications/Partials/JobApplicationModal.vue';
 </script>
 
 <template>
   <MainLayout>
-    <div class="max-w-6xl mx-auto px-8 py-24">
-      <h1>Applications</h1>
+    <div class="max-w-6xl mx-auto px-8 py-12">
+      <div class="flex justify-between items-center">
+        <h3>Your Applications</h3>
+
+        <JobApplicationModal />
+      </div>
     </div>
   </MainLayout>
 </template>
---
+++ resources/js/Pages/Applications/Partials/JobApplicationModal.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Modal from '@/Components/Breeze/Modal.vue';
+import { ref } from 'vue';
+import SecondaryButton from '@/Components/Breeze/SecondaryButton.vue';
+
+const showModal = ref(true);
+</script>
+
+<template>
+  <div>
+    <SecondaryButton @click.prevent="showModal = true">
+      + Application
+    </SecondaryButton>
+    <Modal :show="showModal" @close="showModal = false">
+      <div class="p-6">
+        <h3 class="text-lg font-medium text-neutral-100 mb-1">
+          New Job Application
+        </h3>
+        <p class="text-sm text-neutral-300 mb-6">
+          Please fill out the form below to apply for this job.
+        </p>
+
+        <form class="space-y-6">
+          <h1>form will go here...</h1>
+        </form>
+      </div>
+    </Modal>
+  </div>
+</template>
