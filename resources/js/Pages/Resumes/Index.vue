bac16f20d5e7d7f48fb27758cfb579f7a14330ee
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-11 03:05:27 -0500

    wip

--- resources/js/Pages/Resumes/Index.vue
+++ resources/js/Pages/Resumes/Index.vue
@@ -1,10 +1,16 @@
 <script setup>
 import MainLayout from '@/Layouts/MainLayout.vue';
 import UploadResume from '@/Pages/Resumes/Partials/UploadResume.vue';
+import DownloadIcon from '@/Components/Icons/DownloadIcon.vue';
+import { useForm } from '@inertiajs/vue3';
 
-defineProps({
+const props = defineProps({
   resumes: Array,
 });
+
+const downloadResume = (resume) => {
+  useForm({}).get(route('resumes.download', resume.id));
+};
 </script>
 
 <template>
@@ -21,6 +27,21 @@ defineProps({
           Manage all your resumes here that you wish to use for your job
           applications.
         </p>
+
+        <div
+          v-for="resume in resumes"
+          :key="resume.id"
+          class="bg-neutral-800 shadow rounded-lg p-4 mt-8 hover:shadow-lg transition duration-300 hover:scale-105 cursor-pointer"
+        >
+          <div class="flex justify-between items-center">
+            <div>
+              <h4 class="text-lg font-semibold">{{ resume.title }}</h4>
+              <p class="text-neutral-500">
+                {{ new Date(resume.created_at).toLocaleDateString() }}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </MainLayout>
