9c0d52c42833b612c0e956e7a88ccc05e86a5dc5
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-10 21:17:44 -0500

    view toggle

--- resources/js/Pages/Applications/Show/Page.vue
+++ resources/js/Pages/Applications/Show/Page.vue
@@ -1,11 +1,15 @@
 <script setup>
 import MainLayout from '@/Layouts/MainLayout.vue';
 import ApplicationHeader from '@/Pages/Applications/Index/Partials/ApplicationHeader.vue';
+import ViewToggle from '@/Pages/Applications/Show/Partials/ViewToggle.vue';
 import ContactModal from '@/Pages/Applications/Show/Partials/ContactModal.vue';
+import { ref } from 'vue';
 
 defineProps({
   jobApplication: Object,
 });
+
+const showEditForm = ref(false);
 </script>
 
 <template>
@@ -13,7 +17,13 @@ defineProps({
     <div class="max-w-6xl mx-auto px-8 py-12">
       <div class="flex justify-between items-center">
         <ApplicationHeader :jobApplication="jobApplication" />
-        <ContactModal :jobApplication="jobApplication" />
+        <div class="flex items-center gap-8">
+          <ViewToggle
+            :showEditForm="showEditForm"
+            @toggleView="showEditForm = !showEditForm"
+          />
+          <ContactModal :jobApplication="jobApplication" />
+        </div>
       </div>
     </div>
   </MainLayout>
---
+++ resources/js/Pages/Applications/Show/Partials/ViewToggle.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SecondaryButton from '@/Components/Breeze/SecondaryButton.vue';
+
+defineProps({
+  showEditForm: Boolean,
+});
+
+defineEmits(['toggleView']);
+</script>
+
+<template>
+  <div>
+    <SecondaryButton @click.prevent="$emit('toggleView')">
+      <span v-if="showEditForm">Timeline</span>
+      <span v-else>Edit</span>
+    </SecondaryButton>
+  </div>
+</template>
