abf79346561d260d2c0323018fd26126cebcc29b
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-11 01:59:31 -0500

    resumes scaffolding

--- resources/js/Pages/Resumes/Index.vue
+++ resources/js/Pages/Resumes/Index.vue
@@ -1,9 +1,22 @@
 <script setup>
 import MainLayout from '@/Layouts/MainLayout.vue';
+import SecondaryButton from '@/Components/Breeze/SecondaryButton.vue';
 </script>
 
 <template>
   <MainLayout>
-    <h1>Resumes</h1>
+    <div class="max-w-6xl mx-auto px-8 py-12">
+      <div class="max-w-4xl">
+        <div class="flex justify-between items-center mb-2">
+          <h3 class="text-xl font-semibold">Resumes</h3>
+
+          <SecondaryButton
+            class="bg-orange-400 hover:bg-orange-600 w-full sm:w-fit flex justify-center"
+          >
+            New
+          </SecondaryButton>
+        </div>
+      </div>
+    </div>
   </MainLayout>
 </template>
