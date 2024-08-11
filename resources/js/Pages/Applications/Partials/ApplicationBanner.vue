116104dba03197e74863a9102d030a27f06c5911
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-10 19:10:15 -0500

    responsive banner

--- resources/js/Pages/Applications/Partials/ApplicationBanner.vue
+++ resources/js/Pages/Applications/Partials/ApplicationBanner.vue
@@ -15,25 +15,29 @@ const gotoApplication = () => {
     class="p-6 bg-neutral-800 rounded-md shadow-md transform transition-transform duration-300 hover:scale-105 cursor-pointer"
     @click.prevent="gotoApplication"
   >
-    <div class="flex justify-between items-start">
-      <div>
-        <h2 class="text-2xl font-semibold text-white flex items-center gap-2">
+    <div
+      class="flex flex-col-reverse gap-4 md:flex-row justify-between items-start md:items-center"
+    >
+      <!-- Position and Company Info -->
+      <div class="mb-4 md:mb-0">
+        <h2 class="text-xl font-semibold text-white">
           {{ jobApplication.position }}
         </h2>
-        <p class="text-lg text-gray-400 flex items-center gap-2">
+        <p class="text-lg text-gray-400">
           {{ jobApplication.company_name }}
         </p>
       </div>
 
+      <!-- Status and Last Update -->
       <div
-        class="flex flex-col md:flex-row-reverse justify-center items-center gap-2 md:gap-4"
+        class="w-full flex flex-row md:flex-col justify-between items-center md:items-end text-sm space-y-2"
       >
         <div
-          class="px-3 py-1 text-sm font-semibold rounded-full text-white bg-green-500"
+          class="px-3 py-1 font-semibold rounded-full text-white bg-green-500 text-center"
         >
           {{ jobApplication.status_pretty }}
         </div>
-        <p class="text-gray-400 text-sm">
+        <p class="text-gray-400 text-center md:text-right">
           {{ jobApplication.last_update }}
         </p>
       </div>
