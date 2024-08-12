382cc192cbd8a9cef835916008e95450d7a37073
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-11 20:58:29 -0500

    in progress...

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -4,6 +4,10 @@ import MainLayout from '@/Layouts/MainLayout.vue';
 
 <template>
   <MainLayout>
-    <h1>Dashboard</h1>
+    <div class="max-w-6xl mx-auto px-8 py-12">
+      <p class="text-2xl font-semibold text-gray-700 dark:text-gray-200">
+        Coming soon...
+      </p>
+    </div>
   </MainLayout>
 </template>
