292e5498ed5d621c20a86070131dc6483036a03e
Riley Grotenhuis <riley@Rileys-Air.attlocal.net>
2024-08-13 13:04:25 -0500

    wip

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -13,18 +13,21 @@ defineProps({
 
 <template>
   <MainLayout>
-    <div class="max-w-6xl mx-auto px-8 py-12 flex flex-col md:flex-row gap-8">
-      <div v-if="hasApplications">
-        <div class="flex-1">
-          <UpcomingInterviews :upcomingInterviews="upcomingInterviews" />
-        </div>
-
-        <div class="w-full md:w-1/3">
-          <UpcomingReminders :upcomingReminders="upcomingReminders" />
-        </div>
+    <div
+      v-if="hasApplications"
+      class="max-w-6xl mx-auto px-8 py-12 flex flex-col md:flex-row gap-8"
+    >
+      <div class="flex-1">
+        <UpcomingInterviews :upcomingInterviews="upcomingInterviews" />
       </div>
 
-      <div v-else class="max-w-3xl">
+      <div class="w-full md:w-1/3">
+        <UpcomingReminders :upcomingReminders="upcomingReminders" />
+      </div>
+    </div>
+
+    <div v-else class="max-w-6xl mx-auto px-8 py-12 gap-8">
+      <div class="max-w-3xl">
         <p class="text-neutral-300 font-bold">
           You have no outgoing applications at the moment. Start applying to
           jobs to see your upcoming interviews and reminders.
